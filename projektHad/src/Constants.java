9bf002edc09c5155db3370e44b18a528a4922383
tadea <tadeas.zikl@gmail.com>
2024-05-16 15:30:46 +0200

    Initial commit

---
+++ projektHad/src/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 900;
+    public static final String SCREEN_TITLE = "Snake";
+
+    public static final float TILE_WIDTH = 24;
+}
