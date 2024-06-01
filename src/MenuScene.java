c6e44e4b43eb29d582406578bbecf66159281b26
tadea <tadeas.zikl@gmail.com>
2024-06-01 17:35:38 +0200

    Code design changes.

--- src/MenuScene.java
+++ src/MenuScene.java
@@ -24,10 +24,12 @@ public class MenuScene extends Scene {
      */
 
     public MenuScene(KeyL keyListener, MouseL mouseListener) {
+
         this.keyListener = keyListener;
         this.mouseListener = mouseListener;
 
         try {
+
             BufferedImage spritesheet = ImageIO.read(new File("C:\\Users\\tadea\\Desktop\\projectHad\\snakeMenu.png"));
             title = spritesheet.getSubimage(170, 40, 320, 70);
             play = spritesheet.getSubimage(130, 130, 410, 40);
@@ -45,6 +47,7 @@ public class MenuScene extends Scene {
         titleRect = new Rect(240, 100, 320, 70);
         playRect = new Rect(195, 280, 410, 40);
         exitRect = new Rect(245, 355, 310, 40);
+
     }
 
     /**
@@ -54,6 +57,7 @@ public class MenuScene extends Scene {
 
     @Override
     public void update(double dt) {
+
         if (mouseListener.getX() >= playRect.x && mouseListener.getX() <= playRect.x + playRect.width &&
                 mouseListener.getY() >= playRect.y && mouseListener.getY() <= playRect.y + playRect.height) {
             playCurrentImage = playPressed;
@@ -83,17 +87,20 @@ public class MenuScene extends Scene {
 
     @Override
     public void draw(Graphics g) {
+
         g.setColor(new Color(10, 220, 215));
         g.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
-
         g.drawImage(title, (int)titleRect.x, (int)titleRect.y, (int)titleRect.width, (int)titleRect.height, null);
         g.drawImage(playCurrentImage, (int)playRect.x, (int)playRect.y, (int)playRect.width, (int)playRect.height, null);
         g.drawImage(exitCurrentImage, (int)exitRect.x, (int)exitRect.y, (int)exitRect.width, (int)exitRect.height, null);
+
         Font font = new Font("Tahoma", Font.BOLD, 18);
         FontMetrics metrics = g.getFontMetrics(font);
         String nicknameText = "Your nickname: " + Window.getWindow().nickname;
+
         g.setFont(font);
         g.setColor(Color.BLACK);
         g.drawString(nicknameText, Constants.SCREEN_WIDTH / 2 - (metrics.stringWidth(nicknameText) / 2), Constants.SCREEN_HEIGHT - 50);
+
     }
 }
